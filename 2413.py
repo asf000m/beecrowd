209a908b15f48578659e12707807b84b6a513e0a
asf000m <asf000m@gmail.com>
2026-04-07 21:11:33 -0300

    Problem 2413 - Busca na Internet

---
+++ 2413.py
@@ -0,0 +1,21 @@
+# beecrowd | 2413
+# Busca na Internet
+
+'''
+terceiro link
+t
+
+segundo link
+2 * t
+
+primeiro link
+p / 2 = 2 * t
+p = 4 * t
+4 * t
+'''
+
+terceiro_link = int(input())
+
+primeiro_link = 4 * terceiro_link
+
+print(primeiro_link)
